2d5a583a2284158cae0270e874074afadf91472a
JACK-QBS <2200838299@qq.com>
2021-01-13 17:23:37 +0800

    设置当前进程等待时间5s，t进程运行3秒完毕后，执行当前进程

---
+++ 多线程/进程常见方法的使用/JoinTest2.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+public class JoinTest2 {
+    public static void main(String[] args) throws InterruptedException {
+        // t和main同时并发并行执行，但因为main线程正在运行态执行代码，很快执行后序代码
+        //打印main和t，本来应该乱序随机，但是先打印main概率上非常高
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("t");
+            }
+        });//申请系统创建线程t
+        t.start();//申请系统执行线程t：创建态转变位就绪态，由系统决定什么时候转变位运行态
+        t.join(5000);//当前线程无条件等待，直到t线程执行完毕，当前线程往后执行
+        System.out.println("main");
+    }
+}
