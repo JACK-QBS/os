c4529f7a30a3364f3e1b073c2419e2a2db1caa4f
JACK-QBS <2200838299@qq.com>
2021-01-16 20:28:20 +0800

    st

---
+++ 多线程/进程常见方法的使用/SingleThread.java
@@ -0,0 +1,28 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SingleThread {
+    public static void main(String[] args) {
+        //第一种情况：耗时多的任务
+        calculate(new ArrayList<>());
+        calculate(new ArrayList<>());
+        //第二种情况：阻塞任务
+        Scanner sc = new Scanner(System.in);
+        print(sc);
+        print(sc);
+    }
+
+    public static int calculate(List<Integer> list) {
+        //计算量非常大
+        return 0;
+    }
+
+    public static void print(Scanner sc) {
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
